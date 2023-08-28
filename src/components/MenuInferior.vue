8fa6e03cf0aad7fc7d3c6ba2307c577c942b76bc
Fagner Concolato <fca.fisica@gmail.com>
2023-08-28 17:48:37 -0300

    add:menuInferior

--- src/components/MenuInferior.vue
+++ src/components/MenuInferior.vue
@@ -1,18 +1,29 @@
 <script setup>
 
+const rotaIcones = ()=>{
+   alert('clicou')
+}
 
+const rotaBotaoMais = ()=>{
+  alert('clicou no botao Mais')
+}
 </script>
 
 
 <template>
 
     <footer class="footer">
-
-        <button>dieta</button>
-        <button>exercicio</button>
-        <button>+</button>
-        <button>pessoa</button>
-        <button>carrinhoCompra</button>
+          <!-- perguntar pq nao funciona mesmo tendo bootstrap instalado -->
+        <!-- <button>  -->
+        <!-- <i class="bi-6-square" style="font-size: 120px;"></i>
+      </button>
+        <button><i class="bi bi-0-circle"></i></button> -->
+        <button @click="rotaIcones" class="BotoesBarraInferior"><img src="../assets/imgs/dieta.svg" alt=""></button>
+        <button @click="rotaIcones" class="BotoesBarraInferior"><img  src="../assets/imgs/alter.svg" alt="Alter"></button>
+        <button @click="rotaBotaoMais" class="BotoesBarraInferior botaoMais" ><img src="../assets/imgs/mais.svg" alt="Icone de mais"></button>
+        <button @click="rotaIcones" class="BotoesBarraInferior"><img src="../assets/imgs/user.svg" alt="Usuario"></button>
+        <button @click="rotaIcones" class="BotoesBarraInferior"><img class=" carrinho" src="../assets/imgs/carrinhoCompra.svg" alt="Carrinho de compra"></button>
+         
 
     </footer>
 
@@ -22,8 +33,43 @@
  .footer{
     display: flex;
     justify-content: center;
+    background-color: black; 
+    height: 80px;
+    margin-top: 540px;
  }
-
+ 
+.BotoesBarraInferior{
+  border: none; 
+  background-color: transparent; 
+  padding: 20px;
+}
+ .BotoesBarraInferior:hover{
+  background-color: var(--button-color-light);
+  cursor: pointer;
+  
+} 
+.botaoMais{
+  border-radius: 200px;
+  background-color: var(--button-color-light);  
+}
+.botaoMais:hover{
+  background-color: var(--button-color-light);
+  appearance: 1;
+  
+}
+.carrinho{
+  fill: blueviolet;
+}
+.carrinho:hover{
+ fill: chartreuse;
+  
+}
+.imagem{
+    fill: #626463;
+}
+.imagem:hover{
+  fill: var(--button-color-light);
+}
 
 
 </style>
