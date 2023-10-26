cbae8008c5acde9a471658a63801235bff55050a
Matheus Grzibovski Vieira <101050682+MatheusGrz141@users.noreply.github.com>
2023-10-26 13:17:48 -0400

    Update client.http.js

--- src/api/client.http.js
+++ src/api/client.http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+     baseURL: "https://bestrong-api.up.railway.app/api", 
     timeout: 1000,
     headers: {'Content-Type': 'application/json'}
 });
