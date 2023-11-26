05d58ef4d7493b74fb661aefbab258142ba24744
Joao <zacarias1021@gmail.com>
2023-11-25 23:09:30 -0400

    fix: just name train to meal

--- src/stores/meal.store.js
+++ src/stores/meal.store.js
@@ -28,7 +28,7 @@ export const useMealStore = defineStore('meal', {
       this.setMeal(await userResource.findMeal(payload))
       return await userResource.findMeal(payload)
     },
-    async editTrain(payload) {
+    async editMeal(payload) {
       return await userResource.editMeal(payload)
     },
     async addFood(payload) {
